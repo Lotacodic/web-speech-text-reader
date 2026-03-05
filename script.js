9d0594bd5b2781973b659df00fde0c3ff5dfdb38
lotacodic <ngenejoshua05@gmail.com>
2026-03-06 02:01:36 +0300

    feat: load available voices and populate dropdown dynamically

--- script.js
+++ script.js
@@ -2,3 +2,26 @@
 const voiceSelect = document.getElementById("voice-select");
 const synth = window.speechSynthesis;
 let voices;
+
+function addVoicesToSelect() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} - ${voices[i].lang}`;
+
+    if (voices[i].default) {
+      option.textContent += " - DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+// Ensure voices are loaded correctly
+addVoicesToSelect();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = addVoicesToSelect;
+}
